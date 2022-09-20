bef07231ecb45e69aa4ae0edc48d6bdb3f2137b0
Felipe Somensi Duarte <110866742+FelipeSomensi@users.noreply.github.com>
2022-09-20 18:12:00 -0300

    Create NPC.java

---
+++ NPC.java
@@ -0,0 +1,15 @@
+public class NPC extends Ficha{
+	
+	private static int totalNPCS;
+	
+	public NPC(String nomePersonagem, String tipoDeFicha) {
+		super(nomePersonagem, tipoDeFicha);
+		System.out.println("Condicional criacao de Ficha funcionando");
+		
+		
+	
+	}
+
+	
+	
+}
